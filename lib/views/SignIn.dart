71d34a2bd11b86f81ba43bddfb4ec1887c7cdf66
Ps321 <41200413+Ps321@users.noreply.github.com>
2022-03-21 17:40:36 +0530

    Update SignIn.dart

--- lib/views/SignIn.dart
+++ lib/views/SignIn.dart
@@ -538,3 +538,217 @@ class _ProfileUI2State extends State<ProfileUI2> {
 
 */
 
+
+
+
+
+
+/* Emoji Keyboard
+
+import 'dart:async';
+import 'dart:io';
+
+import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_try1/navigation.dart';
+import 'package:flutter_try1/profile.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+/// Example for EmojiPickerFlutter
+class MyApp extends StatefulWidget {
+
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final TextEditingController _controller = TextEditingController();
+  bool emojiShowing = false;
+
+  Timer? _timer;
+  var focusNode = FocusNode();
+  @override
+  void initState() {
+    focusNode.addListener(() {
+      print(focusNode.hasFocus);
+      if(focusNode.hasFocus) {
+        setState(() {
+          emojiShowing = false;
+        });
+        print("aaya");
+      }
+
+    });
+
+    super.initState();
+  }
+   _onfocuschange(){
+    print("aaya");
+  }
+  _onEmojiSelected(Emoji emoji) {
+    _controller
+      ..text += emoji.emoji
+      ..selection = TextSelection.fromPosition(
+          TextPosition(offset: _controller.text.length));
+  }
+
+  _onBackspacePressed() {
+    _controller
+      ..text = _controller.text.characters.skipLast(1).toString()
+      ..selection = TextSelection.fromPosition(
+          TextPosition(offset: _controller.text.length));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Builder(
+        builder:(context)=> Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            title: const Text('Emoji Picker Example App'),
+          ),
+          body: Column(
+            children: [
+              Expanded(child: Container(
+                child: FlatButton(
+                  onPressed: (){
+                    print("hua");
+                    Navigator.push(context,
+                        MaterialPageRoute(
+                            builder: (context)=>Navigation(
+
+                            )
+                        ));
+                  },
+                  child: Text("New page"),
+
+                ),
+              )),
+              Container(
+                  height: 66.0,
+                  color: Colors.blue,
+                  child: Row(
+                    children: [
+                      Material(
+                        color: Colors.transparent,
+                        child: IconButton(
+                          onPressed: () {
+
+                            if(focusNode.hasFocus){
+
+                              focusNode.unfocus();
+                            }
+                            _timer = new Timer(const Duration(milliseconds: 200), () {
+                              setState(() {
+                                emojiShowing = !emojiShowing;
+                              });
+                            });
+                          },
+                          icon: const Icon(
+                            Icons.emoji_emotions,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8.0),
+                          child: GestureDetector(
+
+                            child: TextField(
+                              focusNode: focusNode,
+
+
+                                controller: _controller,
+                                style: const TextStyle(
+                                    fontSize: 20.0, color: Colors.black87),
+                                decoration: InputDecoration(
+                                  hintText: 'Type a message',
+                                  filled: true,
+                                  fillColor: Colors.white,
+                                  contentPadding: const EdgeInsets.only(
+                                      left: 16.0,
+                                      bottom: 8.0,
+                                      top: 8.0,
+                                      right: 16.0),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(50.0),
+                                  ),
+                                )),
+                          ),
+                        ),
+                      ),
+                      Material(
+                        color: Colors.transparent,
+                        child: IconButton(
+                            onPressed: () {
+                              // send message
+                            },
+                            icon: const Icon(
+                              Icons.send,
+                              color: Colors.white,
+                            )),
+                      )
+                    ],
+                  )),
+              Offstage(
+                offstage: !emojiShowing,
+                child: SizedBox(
+                  height: 250,
+                  child: EmojiPicker(
+                      onEmojiSelected: (Category category, Emoji emoji) {
+                        _onEmojiSelected(emoji);
+                      },
+                      onBackspacePressed: _onBackspacePressed,
+                      config: Config(
+                          columns: 7,
+                          // Issue: https://github.com/flutter/flutter/issues/28894
+                          emojiSizeMax: 32 * (Platform.isIOS ? 1.30 : 1.0),
+                          verticalSpacing: 0,
+                          horizontalSpacing: 0,
+                          initCategory: Category.RECENT,
+                          bgColor: const Color(0xFFF2F2F2),
+                          indicatorColor: Colors.blue,
+                          iconColor: Colors.grey,
+                          iconColorSelected: Colors.blue,
+                          progressIndicatorColor: Colors.blue,
+                          backspaceColor: Colors.blue,
+                          skinToneDialogBgColor: Colors.white,
+                          skinToneIndicatorColor: Colors.grey,
+                          enableSkinTones: true,
+                          showRecentsTab: true,
+                          recentsLimit: 28,
+                          noRecentsText: 'No Recents',
+                          noRecentsStyle: const TextStyle(
+                              fontSize: 20, color: Colors.black26),
+                          tabIndicatorAnimDuration: kTabScrollDuration,
+                          categoryIcons: const CategoryIcons(),
+                          buttonMode: ButtonMode.MATERIAL)),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+/*
+
+
+
+
+
+
+ */
+
+
+*/
+
