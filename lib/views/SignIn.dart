870840b419864b8d626052826498329fcdb17558
Ps321 <41200413+Ps321@users.noreply.github.com>
2022-03-21 17:28:02 +0530

    Update SignIn.dart

--- lib/views/SignIn.dart
+++ lib/views/SignIn.dart
@@ -194,3 +194,347 @@ class _SignInState extends State<SignIn> {
     );
   }
 }
+
+
+/*Navigation
+
+import 'package:flutter/material.dart';
+import 'package:flutter_try1/main.dart';
+import 'package:flutter_try1/profile.dart';
+
+class Navigation extends StatefulWidget {
+  const Navigation({Key? key}) : super(key: key);
+
+  @override
+  _NavigationState createState() => _NavigationState();
+}
+
+class _NavigationState extends State<Navigation> {
+  var _selectedIndex1 = 3;
+  static const List<Widget> _pages = <Widget>[
+    Icon(
+      Icons.call,
+      size: 150,
+    ),
+    Icon(
+      Icons.camera,
+      size: 150,
+    ),
+    Icon(
+      Icons.chat,
+      size: 150,
+    ),
+    ProfileUI2(),
+  ];
+
+  void onItemTapped(int index) {
+    if(index==0){
+      Navigator.push(context, MaterialPageRoute(builder: (context)=>MyApp()));
+    }
+    setState(() {
+      _selectedIndex1 = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+        appBar: AppBar(
+          title: const Text('Emoji Picker Example App'),
+        ),
+        body: Center(
+          child: _pages.elementAt(_selectedIndex1),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+/*
+
+
+ */
+          type: BottomNavigationBarType.shifting,
+          selectedFontSize: 20,
+          selectedIconTheme: IconThemeData(color: Colors.amberAccent),
+          selectedItemColor: Colors.amberAccent,
+          selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.message,color: Colors.black,),
+              label: 'Message',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search,color: Colors.black,),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.notification_important_outlined,color: Colors.black,),
+              label: 'Notification',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person,color: Colors.black,),
+              label: 'Profile',
+            ),
+          ],
+          currentIndex: _selectedIndex1, //New
+          onTap: onItemTapped, //New
+        )
+//New
+
+        );
+  }
+}
+
+
+
+
+*/
+
+
+
+
+
+
+/* Profile.dart
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ProfileUI2 extends StatefulWidget {
+  const ProfileUI2({Key? key}) : super(key: key);
+
+
+  @override
+  _ProfileUI2State createState() => _ProfileUI2State();
+}
+
+class _ProfileUI2State extends State<ProfileUI2> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+        body: SingleChildScrollView(
+          child: SafeArea(
+            child: Column(
+
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              "add you image URL here "
+                          ),
+                          fit: BoxFit.cover
+                      )
+                  ),
+                  child: Container(
+                    width: double.infinity,
+                    height: 200,
+                    child: Container(
+                      alignment: Alignment(0.0,2.5),
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            "Add you profile DP image URL here "
+                        ),
+                        radius: 60.0,
+                      ),
+                    ),
+                  ),
+                ),
+
+                SizedBox(
+                  height: 60,
+                ),
+                Text(
+                  "Rajat Palankar"
+                  ,style: TextStyle(
+                    fontSize: 25.0,
+                    color:Colors.blueGrey,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.w400
+                ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Belgaum, India"
+                  ,style: TextStyle(
+                    fontSize: 18.0,
+                    color:Colors.black45,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.w300
+                ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "App Developer at XYZ Company"
+                  ,style: TextStyle(
+                    fontSize: 15.0,
+                    color:Colors.black45,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.w300
+                ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Card(
+                    margin: EdgeInsets.symmetric(horizontal: 20.0,vertical: 8.0),
+
+                    elevation: 2.0,
+                    child: Padding(
+                        padding: EdgeInsets.symmetric(vertical: 12,horizontal: 30),
+                        child: Text("Skill Sets",style: TextStyle(
+                            letterSpacing: 2.0,
+                            fontWeight: FontWeight.w300
+                        ),))
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Text(
+                  "App Developer || Digital Marketer"
+                  ,style: TextStyle(
+                    fontSize: 18.0,
+                    color:Colors.black45,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.w300
+                ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(horizontal: 20.0,vertical: 8.0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Expanded(
+                          child: Column(
+                            children: [
+                              Text("Project",
+                                style: TextStyle(
+                                    color: Colors.blueAccent,
+                                    fontSize: 22.0,
+                                    fontWeight: FontWeight.w600
+                                ),),
+                              SizedBox(
+                                height: 7,
+                              ),
+                              Text("15",
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 22.0,
+                                    fontWeight: FontWeight.w300
+                                ),)
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          child:
+                          Column(
+                            children: [
+                              Text("Followers",
+                                style: TextStyle(
+                                    color: Colors.blueAccent,
+                                    fontSize: 22.0,
+                                    fontWeight: FontWeight.w600
+                                ),),
+                              SizedBox(
+                                height: 7,
+                              ),
+                              Text("2000",
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 22.0,
+                                    fontWeight: FontWeight.w300
+                                ),)
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    RaisedButton(
+                      onPressed: (){
+
+                      },
+                      shape:  RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(80.0),
+                      ),
+                      child: Ink(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                              begin: Alignment.centerLeft,
+                              end: Alignment.centerRight,
+                              colors: [Colors.pink,Colors.redAccent]
+                          ),
+                          borderRadius: BorderRadius.circular(30.0),
+
+                        ),
+                        child: Container(
+                          constraints: BoxConstraints(maxWidth: 100.0,maxHeight: 40.0,),
+                          alignment: Alignment.center,
+                          child: Text(
+                            "Contact me",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 12.0,
+                                letterSpacing: 2.0,
+                                fontWeight: FontWeight.w300
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    RaisedButton(
+                      onPressed: (){
+
+                      },
+                      shape:  RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(80.0),
+                      ),
+                      child: Ink(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                              begin: Alignment.centerLeft,
+                              end: Alignment.centerRight,
+                              colors: [Colors.pink,Colors.redAccent]
+                          ),
+                          borderRadius: BorderRadius.circular(80.0),
+
+                        ),
+                        child: Container(
+                          constraints: BoxConstraints(maxWidth: 100.0,maxHeight: 40.0,),
+                          alignment: Alignment.center,
+                          child: Text(
+                            "Portfolio",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 12.0,
+                                letterSpacing: 2.0,
+                                fontWeight: FontWeight.w300
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        )
+    );
+  }
+}
+
+
+*/
+
